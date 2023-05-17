831c544191edef9364312897c51362e7c319a15c
mac <mac@192.168.249.1>
2023-05-17 14:39:44 +0530

    learn-spring-boot Basics of springboot

---
+++ learn-spring-boot/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into COURSE (ID,AUTHOR,NAME)
+values (100001,'in28minutes','learn springboot');  
+insert into COURSE (ID,AUTHOR,NAME)
+values (100002,'in28minutes','learn fullstack');
+insert into COURSE (ID,AUTHOR,NAME)
+values (100003,'in28minutes','learn cloud');    
